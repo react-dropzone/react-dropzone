76f709aaf5d970ebdce858520b3d6c550d3ad786
Subtirelu Mihail <dazzzz2007@gmail.com>
2015-02-03 16:40:53 +0200

    Added multiple files option and input fallback options as params

--- index.js
+++ index.js
@@ -4,14 +4,25 @@
 
 var React = require('react');
 
+/**
+ * Props:
+ *
+ *  --- className: In case you want to give a custom class name to the dropzone and style it
+ *
+ *  --- inputFile: In case you want a fallback for your drop zone
+ *
+ *  --- inputFileMultiple: in case you want multiple files
+ *
+ */
+
 var Dropzone = React.createClass({
   getInitialState: function() {
     return {
       isDragActive: false
-    }
+    };
   },
 
-  handleDragLeave: function(e) {
+  handleDragLeave: function() {
     this.setState({
       isDragActive: false
     });
@@ -26,32 +37,8 @@ var Dropzone = React.createClass({
     });
   },
 
-  handleClick: function(e) {
-    e.preventDefault();
-    var event = new MouseEvent('click', {
-     'view': window,
-     'bubbles': true,
-     'cancelable': false
-    });
-    var node = e.target.getElementsByTagName('input')[0];
-    if (node) {
-      node.dispatchEvent(event);
-    }
-  },
-
-  handleChange: function(e) {
-    e.preventDefault();
-
-    this.setState({
-      isDragActive: false
-    });
-
-    if (this.props.handler) {
-      var file = e.target && e.target.files && e.target.files[0];
-      this.props.handler(file);
-    } else {
-      console.error('No handler specified to accept the file.');
-    }
+  handleFileSelect: function(e){
+    this.props.handler(e.target.files);
   },
 
   handleDrop: function(e) {
@@ -62,47 +49,68 @@ var Dropzone = React.createClass({
     });
 
     if (this.props.handler) {
-      var file = e.dataTransfer && e.dataTransfer.files && e.dataTransfer.files[0];
-      this.props.handler(file);
+      var files = this.props.inputFileMultiple ?
+        e.dataTransfer && e.dataTransfer.files :
+        [e.dataTransfer && e.dataTransfer.files && e.dataTransfer.files[0]];
+
+      this.props.handler(files);
     } else {
       console.error('No handler specified to accept the dropped file.');
     }
   },
+
+  createInputFile: function(){
+    if(!this.props.inputFile){
+      return false;
+    }
+
+    var inputConstruct = [];
+
+    var DropzoneInput = {
+      margin: '15px auto',
+      width: '85px'
+    };
+
+    inputConstruct.push(<div><br/>or</div>);
+
+    if(this.props.inputFileMultiple){
+        inputConstruct.push(<input type="file" name="file" style={DropzoneInput} className="Dropzone-Input" multiple onChange={this.handleFileSelect}/>);
+    }else{
+        inputConstruct.push(<input type="file" name="file" style={DropzoneInput} className="Dropzone-Input" onChange={this.handleFileSelect}/>);
+    }
+
+    return inputConstruct;
+  },
+
   render: function() {
 
-    var size = this.props.size || "100pt";
-      var dropzoneStyle = this.props.children ? {} : {
-        width: size,
-        height: size,
-        borderRadius: "10%",
-        borderWidth: "2pt",
-        borderColor: "#666",
-        borderStyle: this.state.isDragActive ? "solid" : "dashed"
-      };
-      dropzoneStyle.overflow = "hidden";
-      dropzoneStyle.cursor = "pointer";
+    var borderColor = this.state.isDragActive ? 'green' : 'lightgray';
 
-      var messageStyle = {
-        display: "table-cell",
-        width: size,
-        height: size,
-        textAlign: "center",
-        verticalAlign: "middle",
-        fontSize: "10pt",
-        textTransform: "uppercase",
-        color: "#666"
-      };
+    var DropzoneStyle = {
+      textAlign: 'center',
+      display: 'block',
+      padding: '80px',
+      border: '3px dashed',
+      borderColor: borderColor,
+      fontSize: '15px'
+    };
 
-      var inputStyle = {
-        position: "absolute",
-        top: "-100px"
-      };
+     var DropzoneText = {
+      textAlign: 'center',
+      padding: '10px 20px',
+      backgroundColor: '#4376DB',
+      color: '#fff',
+      fontSize: '18px',
+      borderRadius: '5px'
+    };
+
+
 
     return (
-      <form className="dropzone" style={dropzoneStyle} onClick={this.handleClick} onChange={this.handleChange} onDragLeave={this.handleDragLeave} onDragOver={this.handleDragOver} onDrop={this.handleDrop}>
-        <input type="file" style={inputStyle}/>
-        {this.props.children || <span style={messageStyle}>{this.props.message || "Drop Here"}</span>}
-      </form>
+      <div className={this.props.className} style={this.props.className ? '' : DropzoneStyle} onDragLeave={this.handleDragLeave} onDragOver={this.handleDragOver} onDrop={this.handleDrop}>
+          {this.props.children || <span style={DropzoneText}>Drop Here</span> }
+          {this.createInputFile()}
+      </div>
     );
   }
 
--- index.jsx
+++ index.jsx
@@ -4,14 +4,25 @@
 
 var React = require('react');
 
+/**
+ * Props:
+ *
+ *  --- className: In case you want to give a custom class name to the dropzone and style it
+ *
+ *  --- inputFile: In case you want a fallback for your drop zone
+ *
+ *  --- inputFileMultiple: in case you want multiple files
+ *
+ */
+
 var Dropzone = React.createClass({
   getInitialState: function() {
     return {
       isDragActive: false
-    }
+    };
   },
 
-  handleDragLeave: function(e) {
+  handleDragLeave: function() {
     this.setState({
       isDragActive: false
     });
@@ -26,6 +37,10 @@ var Dropzone = React.createClass({
     });
   },
 
+  handleFileSelect: function(e){
+    this.props.handler(e.target.files);
+  },
+
   handleDrop: function(e) {
     e.preventDefault();
 
@@ -34,38 +49,67 @@ var Dropzone = React.createClass({
     });
 
     if (this.props.handler) {
-      var file = e.dataTransfer && e.dataTransfer.files && e.dataTransfer.files[0];
-      this.props.handler(file);
+      var files = this.props.inputFileMultiple ?
+        e.dataTransfer && e.dataTransfer.files :
+        [e.dataTransfer && e.dataTransfer.files && e.dataTransfer.files[0]];
+
+      this.props.handler(files);
     } else {
       console.error('No handler specified to accept the dropped file.');
     }
   },
+
+  createInputFile: function(){
+    if(!this.props.inputFile){
+      return false;
+    }
+
+    var inputConstruct = [];
+
+    var DropzoneInput = {
+      margin: '15px auto',
+      width: '85px'
+    };
+
+    inputConstruct.push(<div><br/>or</div>);
+
+    if(this.props.inputFileMultiple){
+        inputConstruct.push(<input type="file" name="file" style={DropzoneInput} className="Dropzone-Input" multiple onChange={this.handleFileSelect}/>);
+    }else{
+        inputConstruct.push(<input type="file" name="file" style={DropzoneInput} className="Dropzone-Input" onChange={this.handleFileSelect}/>);
+    }
+
+    return inputConstruct;
+  },
+
   render: function() {
 
-    var size = this.props.size || "100pt";
-      var dropzoneStyle = this.props.children ? {} : {
-        width: size,
-        height: size,
-        borderRadius: "10%",
-        borderWidth: "2pt",
-        borderColor: "#666",
-        borderStyle: this.state.isDragActive ? "solid" : "dashed"
-      };
-   
-      var messageStyle = {
-        display: "table-cell",
-        width: size,
-        height: size,
-        textAlign: "center",
-        verticalAlign: "middle",
-        fontSize: "10pt",
-        textTransform: "uppercase",
-        color: "#666"
-      };
+    var borderColor = this.state.isDragActive ? 'green' : 'lightgray';
+
+    var DropzoneStyle = {
+      textAlign: 'center',
+      display: 'block',
+      padding: '80px',
+      border: '3px dashed',
+      borderColor: borderColor,
+      fontSize: '15px'
+    };
+
+     var DropzoneText = {
+      textAlign: 'center',
+      padding: '10px 20px',
+      backgroundColor: '#4376DB',
+      color: '#fff',
+      fontSize: '18px',
+      borderRadius: '5px'
+    };
+
+
 
     return (
-      <div className="dropzone" style={dropzoneStyle} onDragLeave={this.handleDragLeave} onDragOver={this.handleDragOver} onDrop={this.handleDrop}>
-        {this.props.children || <span style={messageStyle}>{this.props.message || "Drop Here"}</span>}
+      <div className={this.props.className} style={this.props.className ? '' : DropzoneStyle} onDragLeave={this.handleDragLeave} onDragOver={this.handleDragOver} onDrop={this.handleDrop}>
+          {this.props.children || <span style={DropzoneText}>Drop Here</span> }
+          {this.createInputFile()}
       </div>
     );
   }
