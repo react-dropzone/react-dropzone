8e0f26bd8a89f4c53da8a91bb9964414e9982ad1
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2015-10-20 12:14:28 +0200

    Better code formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 {
-	module.exports = {
-	    entry: "./lib/index.js",
-	    output: {
-	        path: __dirname,
-	        filename: "./dist/react-dropzone.js",
-	        libraryTarget: "var",
-	        library: "Dropzone"
-	    },
-	    externals: {
-	    	"react": "React"
-	    }
-	}
+  module.exports = {
+    entry: "./lib/index.js",
+    output: {
+      path: __dirname,
+      filename: "./dist/react-dropzone.js",
+      libraryTarget: "var",
+      library: "Dropzone"
+    },
+    externals: {
+      "react": "React"
+    }
+  }
 };
