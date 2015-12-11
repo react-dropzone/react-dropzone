7f4c7871f8af33f561b15f738a219012ad412090
Frank Wallis <fwallis@outlook.com>
2015-12-11 19:48:46 +0000

    fix for disablePreview property not being obeyed

---
+++ mocha-environment.js
@@ -0,0 +1,18 @@
+// import es6
+require('babel-core/register');
+
+// jsdom
+var jsdom = require('jsdom');
+
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = global.document.defaultView;
+global.navigator = global.window.navigator;
+
+global.window.URL = {
+  createObjectURL: function(arg) {
+    return "data://" + arg.name;
+  }
+}
+global.window.addEventListener('load', function() {
+    console.log('JSDOM Loaded');
+});
