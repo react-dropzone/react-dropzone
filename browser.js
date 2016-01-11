b9c634ee745b48a3fa16f7c9316628c1fa402bbe
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-01-11 11:37:43 +0100

    Use ES2105 syntax in demo

--- browser.js
+++ browser.js
@@ -1,73 +1,79 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Dropzone = require('react-dropzone');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Dropzone from 'react-dropzone';
 
-var DropzoneDemo = React.createClass({
-    getInitialState: function () {
-      return {
-        files: []
-      };
-    },
+const style = {
+    borderWidth: 2,
+    borderColor: 'black',
+    borderStyle: 'dashed',
+    borderRadius: 4,
+    margin: 30,
+    padding: 30,
+    width: 200,
+    transition: 'all 0.5s'
+};
 
-    onDrop: function (files) {
-      console.log('Received files: ', files);
-      this.setState({
-        files: files
-      });
-    },
+const activeStyle = {
+    borderStyle: 'solid',
+    backgroundColor: '#eee',
+    borderRadius: 8
+};
 
-    showFiles: function () {
-      var files = this.state.files;
+class DropzoneDemo extends React.Component {
 
-      if (files.length <= 0) {
-        return '';
-      }
-
-      return (
-        <div>
-          <h3>Dropped files: </h3>
-          <ul>
-            {[].map.call(files, function (f, i) {
-              return (
-                <li key={i}>
-                  <img src={f.preview} width={100}/>
-                  <div>{f.name + ' : ' + f.size + ' bytes.'}</div>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-        );
-    },
-
-    render: function () {
-      var style = {
-        borderWidth: 2,
-        borderColor: 'black',
-        borderStyle: 'dashed',
-        borderRadius: 4,
-        margin: 30,
-        padding: 30,
-        width: 200,
-        transition: 'all 0.5s'
-      };
-
-      var activeStyle = {
-        borderStyle: 'solid',
-        backgroundColor: '#eee',
-        borderRadius: 8
-      };
-
-      return (
-          <div>
-            <Dropzone onDrop={this.onDrop} style={style} activeStyle={activeStyle}>
-              Try dropping some files here, or click to select files to upload.
-            </Dropzone>
-            {this.showFiles()}
-          </div>
-      );
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            files: []
+        };
     }
-});
+
+    onDrop(files) {
+        console.log('Received files: ', files);
+        this.setState({
+            files: files
+        });
+    }
+
+    showFiles() {
+        const { files } = this.state;
+
+        if (!files.length) {
+            return null;
+        }
+
+        return (
+            <div>
+                <h3>Dropped files: </h3>
+                <ul>
+                    {
+                        files.map((file, idx) => {
+                            return (
+                                <li key={idx}>
+                                    <img src={file.preview} width={100}/>
+                                    <div>{file.name + ' : ' + file.size + ' bytes.'}</div>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <Dropzone onDrop={this.onDrop.bind(this)}
+                          style={style}
+                          activeStyle={activeStyle}>
+                    Try dropping some files here, or click to select files to upload.
+                </Dropzone>
+                {this.showFiles()}
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(<DropzoneDemo />, document.getElementById('example'));
 
--- script.js
+++ script.js
@@ -1,92 +1,122 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Dropzone = require('react-dropzone');
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-var DropzoneDemo = React.createClass({
-  displayName: 'DropzoneDemo',
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
-  getInitialState: function getInitialState() {
-    return {
-      files: []
-    };
-  },
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
-  onDrop: function onDrop(files) {
-    console.log('Received files: ', files);
-    this.setState({
-      files: files
-    });
-  },
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
-  showFiles: function showFiles() {
-    var files = this.state.files;
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-    if (files.length <= 0) {
-      return '';
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = require('react-dom');
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+var _reactDropzone = require('react-dropzone');
+
+var _reactDropzone2 = _interopRequireDefault(_reactDropzone);
+
+var style = {
+    borderWidth: 2,
+    borderColor: 'black',
+    borderStyle: 'dashed',
+    borderRadius: 4,
+    margin: 30,
+    padding: 30,
+    width: 200,
+    transition: 'all 0.5s'
+};
+
+var activeStyle = {
+    borderStyle: 'solid',
+    backgroundColor: '#eee',
+    borderRadius: 8
+};
+
+var DropzoneDemo = (function (_React$Component) {
+    _inherits(DropzoneDemo, _React$Component);
+
+    function DropzoneDemo(props, context) {
+        _classCallCheck(this, DropzoneDemo);
+
+        _get(Object.getPrototypeOf(DropzoneDemo.prototype), 'constructor', this).call(this, props, context);
+        this.state = {
+            files: []
+        };
     }
 
-    return React.createElement(
-      'div',
-      null,
-      React.createElement(
-        'h3',
-        null,
-        'Dropped files: '
-      ),
-      React.createElement(
-        'ul',
-        null,
-        [].map.call(files, function (f, i) {
-          return React.createElement(
-            'li',
-            { key: i },
-            React.createElement('img', { src: f.preview, width: 100 }),
-            React.createElement(
-              'div',
-              null,
-              f.name + ' : ' + f.size + ' bytes.'
-            )
-          );
-        })
-      )
-    );
-  },
+    _createClass(DropzoneDemo, [{
+        key: 'onDrop',
+        value: function onDrop(files) {
+            console.log('Received files: ', files);
+            this.setState({
+                files: files
+            });
+        }
+    }, {
+        key: 'showFiles',
+        value: function showFiles() {
+            var files = this.state.files;
 
-  render: function render() {
-    var style = {
-      borderWidth: 2,
-      borderColor: 'black',
-      borderStyle: 'dashed',
-      borderRadius: 4,
-      margin: 30,
-      padding: 30,
-      width: 200,
-      transition: 'all 0.5s'
-    };
+            if (!files.length) {
+                return null;
+            }
 
-    var activeStyle = {
-      borderStyle: 'solid',
-      backgroundColor: '#eee',
-      borderRadius: 8
-    };
+            return _react2['default'].createElement(
+                'div',
+                null,
+                _react2['default'].createElement(
+                    'h3',
+                    null,
+                    'Dropped files: '
+                ),
+                _react2['default'].createElement(
+                    'ul',
+                    null,
+                    files.map(function (file, idx) {
+                        return _react2['default'].createElement(
+                            'li',
+                            { key: idx },
+                            _react2['default'].createElement('img', { src: file.preview, width: 100 }),
+                            _react2['default'].createElement(
+                                'div',
+                                null,
+                                file.name + ' : ' + file.size + ' bytes.'
+                            )
+                        );
+                    })
+                )
+            );
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            return _react2['default'].createElement(
+                'div',
+                null,
+                _react2['default'].createElement(
+                    _reactDropzone2['default'],
+                    { onDrop: this.onDrop.bind(this),
+                        style: style,
+                        activeStyle: activeStyle },
+                    'Try dropping some files here, or click to select files to upload.'
+                ),
+                this.showFiles()
+            );
+        }
+    }]);
 
-    return React.createElement(
-      'div',
-      null,
-      React.createElement(
-        Dropzone,
-        { onDrop: this.onDrop, style: style, activeStyle: activeStyle },
-        'Try dropping some files here, or click to select files to upload.'
-      ),
-      this.showFiles()
-    );
-  }
-});
+    return DropzoneDemo;
+})(_react2['default'].Component);
 
-ReactDOM.render(React.createElement(DropzoneDemo, null), document.getElementById('example'));
+_reactDom2['default'].render(_react2['default'].createElement(DropzoneDemo, null), document.getElementById('example'));
 
 module.exports = DropzoneDemo;
 
