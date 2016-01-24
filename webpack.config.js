742490d3e1bf2b494bef60936ac43be13726437e
Ville Lindholm <ville.lindholm@iki.fi>
2016-01-24 13:14:59 +0200

    Update build environment (#112)
    Tweaked the build environment:
    
    * Upgraded to babel6.
    * Removed the "loose" parameter in babel, since it's not recommended.
    Testing shows no  behaviour difference in the resulting code.
    * Added a webpack step to generate a ES5 and <script> friendly module
    (webpack is also a devDependency now).
    * Added a uglifyjs step to minify the bundle (uglifyjs also a
    devDependency now)
    
    react-dropzone can now be hosted on a CDN and used directly in a
    <script> tag, although the package attr-accept must also be referenced.
    Maybe bake it into the code with webpack?

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,12 @@
-{
-  module.exports = {
-    entry: "./lib/index.js",
-    output: {
-      path: __dirname,
-      filename: "./dist/react-dropzone.js",
-      libraryTarget: "var",
-      library: "Dropzone"
-    },
-    externals: {
-      "react": "React"
-    }
-  }
+module.exports = {
+	entry: "./lib/index.js",
+	output: {
+		path: __dirname,
+		filename: "./dist/react-dropzone.js",
+		libraryTarget: "var",
+		library: "Dropzone"
+	},
+	externals: {
+		"react": "React"
+	}
 };
