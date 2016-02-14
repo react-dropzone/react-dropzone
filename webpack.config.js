1ce56da6fc9e899ef450ef54111913b2a206383c
Ville Lindholm <ville.lindholm@iki.fi>
2016-02-14 22:37:10 +0200

    Cosmetic improvements
    Fixed indentation, tidied up path and filename strings.

--- webpack.config.js
+++ webpack.config.js
@@ -3,37 +3,37 @@ var path = require('path');
 
 module.exports = {
 	entry: "./src/index.js",
-    devtool: "source-map",
+	devtool: "source-map",
 	output: {
-		path: __dirname,
-		filename: "./dist/react-dropzone.js",
+		path: __dirname + '/dist/',
+		filename: "index.js",
 		libraryTarget: "umd",
 		library: "Dropzone"
 	},
-    module: {
-        loaders: [
-            {
-                include: [
-                    path.resolve(__dirname,"src"),
-                ],
-                test: /\.js$/,
-                loader: 'babel-loader',
-            }
-        ]  
-    },
-    resolve: {
-        // Can require('file') instead of require('file.js') etc.
-        extensions: ['','.js','.json']
-    },
-	externals: {
-		"react": "React"
+	module: {
+		loaders: [
+			{
+				include: [
+					path.resolve(__dirname,"src"),
+				],
+				test: /\.js$/,
+				loader: 'babel-loader',
+			}
+		]
 	},
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                // does this actually compress anything?
-                warnings: false
-            }
-        }),
-    ]
+	resolve: {
+		// Can require('file') instead of require('file.js') etc.
+		extensions: ['','.js','.json']
+	},
+	externals: {
+		"react": "React",
+	},
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				// does this actually compress anything?
+				warnings: false
+			}
+		}),
+	]
 };
