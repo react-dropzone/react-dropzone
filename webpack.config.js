fac1c53a7583eacd88748ad0b612f2aa3151df82
Ville Lindholm <ville.lindholm@iki.fi>
2016-02-14 22:01:47 +0200

    Update PR from feedback
    Export as UMD module, place generated files in "dist", and use webpack
    for the entire build process.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,43 @@
+var webpack = require('webpack');
+var path = require('path');
+
 module.exports = {
-	entry: "./lib/index.js",
+	entry: "./src/index.js",
+    devtool: "source-map",
 	output: {
 		path: __dirname,
 		filename: "./dist/react-dropzone.js",
-		libraryTarget: "var",
+		libraryTarget: "umd",
 		library: "Dropzone"
 	},
+    module: {
+        loaders: [
+            {
+                include: [
+                    path.resolve(__dirname,"src"),
+                ],
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015','react','stage-0'],
+                    plugins: ['add-module-exports']
+                }
+            }
+        ]  
+    },
+    resolve: {
+        // Can require('file') instead of require('file.js') etc.
+        extensions: ['','.js','.json']
+    },
 	externals: {
 		"react": "React"
-	}
+	},
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                // does this actually do anything?
+                warnings: false
+            }
+        }),
+    ]
 };
