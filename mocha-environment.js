826aa6924004527bf3cb78b6f0a84560325c8b41
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-04-04 12:04:12 +0200

    Fixed linting errors

--- mocha-environment.js
+++ mocha-environment.js
@@ -2,17 +2,17 @@
 require('babel-core/register');
 
 // jsdom
-var jsdom = require('jsdom');
+var jsdom = require('jsdom'); // eslint-disable-line
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
 global.navigator = global.window.navigator;
 
 global.window.URL = {
-  createObjectURL: function(arg) {
-    return "data://" + arg.name;
+  createObjectURL: function (arg) { // eslint-disable-line
+    return 'data://' + arg.name;
   }
-}
-global.window.addEventListener('load', function() {
-    console.log('JSDOM Loaded');
+};
+global.window.addEventListener('load', function () {
+  console.log('JSDOM Loaded'); // eslint-disable-line
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
-var webpack = require('webpack');
-var path = require('path');
+var webpack = require('webpack');  // eslint-disable-line
+var path = require('path');  // eslint-disable-line
 
 module.exports = {
-	entry: "./src/index.js",
-	devtool: "source-map",
-	output: {
-		path: __dirname + '/dist/',
-		filename: "index.js",
-		libraryTarget: "umd",
-		library: "Dropzone"
-	},
-	module: {
-		loaders: [
-			{
-				include: [
-					path.resolve(__dirname,"src"),
-				],
-				test: /\.js$/,
-				loader: 'babel-loader',
-			}
-		]
-	},
-	resolve: {
-		// Can require('file') instead of require('file.js') etc.
-		extensions: ['','.js','.json']
-	},
-	externals: {
-		"react": "React",
-	},
-	plugins: [
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				// does this actually compress anything?
-				warnings: false
-			}
-		}),
-	]
+  entry: './src/index.js',
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/dist/',
+    filename: 'index.js',
+    libraryTarget: 'umd',
+    library: 'Dropzone'
+  },
+  module: {
+    loaders: [
+      {
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    // Can require('file') instead of require('file.js') etc.
+    extensions: ['', '.js', '.json']
+  },
+  externals: {
+    react: 'React'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        // does this actually compress anything?
+        warnings: false
+      }
+    })
+  ]
 };
