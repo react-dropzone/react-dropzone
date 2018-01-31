296a75b186c5299c189c224fccc65560bec1db6b
Thaddeus Stewart <thad@truqcapp.com>
2018-01-31 17:44:18 -0500

    fix: make TravisCI happy

--- src/index.js
+++ src/index.js
@@ -398,7 +398,7 @@ class Dropzone extends React.Component {
       <div
         className={className}
         style={appliedStyle}
-        { ...divProps /* expand user provided props first so event handlers are never overridden */ }
+        {...divProps /* expand user provided props first so event handlers are never overridden */}
         onClick={this.composeHandlers(this.onClick)}
         onDragStart={this.composeHandlers(this.onDragStart)}
         onDragEnter={this.composeHandlers(this.onDragEnter)}
@@ -410,7 +410,7 @@ class Dropzone extends React.Component {
       >
         {this.renderChildren(children, isDragActive, isDragAccept, isDragReject)}
         <input
-          { ...inputProps /* expand user provided inputProps first so inputAttributes override them */ }
+          {...inputProps /* expand user provided inputProps first so inputAttributes override them */}
           {...inputAttributes}
         />
       </div>
