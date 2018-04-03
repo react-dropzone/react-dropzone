502b5d5aec35e94512a530a0931f4a2109bc9a37
Patrick Arminio <patrick.arminio@gmail.com>
2018-04-03 22:59:47 +0100

    feat: Add initial typescript definition

---
+++ typings/tests/basic.tsx
@@ -0,0 +1,82 @@
+import * as React from "react";
+
+import Dropzone from "../../";
+
+class Basic extends React.Component {
+  state = { files: [] };
+
+  onDrop(files) {
+    this.setState({
+      files
+    });
+  }
+
+  render() {
+    return (
+      <section>
+        <div className="dropzone">
+          <Dropzone onDrop={this.onDrop.bind(this)}>
+            <p>
+              Try dropping some files here, or click to select files to upload.
+            </p>
+          </Dropzone>
+        </div>
+        <aside>
+          <h2>Dropped files</h2>
+          <ul>
+            {this.state.files.map(f => (
+              <li key={f.name}>
+                {f.name} - {f.size} bytes
+              </li>
+            ))}
+          </ul>
+        </aside>
+      </section>
+    );
+  }
+}
+
+class Basic2 extends React.Component {
+  state = { disabled: true, files: [] };
+
+  onDrop(files) {
+    this.setState({
+      files
+    });
+  }
+
+  render() {
+    return (
+      <section>
+        <aside>
+          <button
+            type="button"
+            onClick={() => this.setState({ disabled: !this.state.disabled })}
+          >
+            Toggle disabled
+          </button>
+        </aside>
+        <div className="dropzone">
+          <Dropzone
+            onDrop={this.onDrop.bind(this)}
+            disabled={this.state.disabled}
+          >
+            <p>
+              Try dropping some files here, or click to select files to upload.
+            </p>
+          </Dropzone>
+        </div>
+        <aside>
+          <h2>Dropped files</h2>
+          <ul>
+            {this.state.files.map(f => (
+              <li>
+                {f.name} - {f.size} bytes
+              </li>
+            ))}
+          </ul>
+        </aside>
+      </section>
+    );
+  }
+}
---
+++ typings/tests/events.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+import Dropzone from "../../";
+
+class Events extends React.Component {
+  render() {
+    return (
+      <section>
+        <div className="dropzone">
+          <Dropzone
+            onDrop={(acceptedFiles, rejectedFiles, event) =>
+              console.log(acceptedFiles, rejectedFiles, event)}
+            onDragStart={event => console.log(event)}
+            onDragEnter={event => console.log(event)}
+            onDragOver={event => console.log(event)}
+            onDragLeave={event => console.log(event)}
+          >
+            <p>
+              Try dropping some files here, or click to select files to upload.
+            </p>
+          </Dropzone>
+        </div>
+      </section>
+    );
+  }
+}
