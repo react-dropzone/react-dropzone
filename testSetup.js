6a76d4e6a3b12d9173270f4c04b23b4e7b4f5943
Tobias Lengsholz <tobias.lengsholz@invision.de>
2018-08-24 16:28:19 +0200

    fix: Use consistent import syntax

--- testSetup.js
+++ testSetup.js
@@ -1,8 +1,8 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import 'jest-enzyme'
 
 Enzyme.configure({ adapter: new Adapter() })
-require('jest-enzyme')
 
 global.window.URL = {
   createObjectURL: function createObjectURL(arg) {
