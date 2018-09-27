343d41f8783cdcfa83921ef647f9cd3f74553bea
Dimitri Ivashchuk <dimaiv@me.com>
2018-09-27 22:58:03 +0200

    docs: Add en example of folders drag'n'drop (#662)
    Closes #661

--- styleguide.config.js
+++ styleguide.config.js
@@ -43,6 +43,22 @@ module.exports = {
         {
           name: 'Full Screen Dropzone',
           content: 'examples/Fullscreen/Readme.md'
+        },
+        {
+          name: 'Extending Dropzone',
+          context: {
+            Dropzone: './src/index'
+          },
+          sections: [
+            {
+              name: 'Using third-party plugins',
+              content: 'examples/PluginArchitecture/Readme.md'
+            },
+            {
+              name: 'Dropzone for folders',
+              content: 'examples/Folders/Readme.md'
+            }
+          ]
         }
       ]
     }
