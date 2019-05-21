93bded79f01641aaf5c4d29642a881cdc0e77bcf
Michael James <majames91@gmail.com>
2019-05-21 14:28:19 +1000

    fix: change types to support custom plugins

--- typings/tests/basic.tsx
+++ typings/tests/basic.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Dropzone from "../../";
+import { FileWithPath } from "file-selector";
 
 export default class Basic extends React.Component {
   state = { files: [] };
 
-  onDrop = files => {
+  onDrop = (files: FileWithPath[]) => {
     this.setState({
       files
     });
