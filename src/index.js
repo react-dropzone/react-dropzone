697af43df3f5b06a5e4108c1e0bddc9fca08bcef
Norton Wong <norton.wong@genedata.com>
2019-10-25 18:44:20 -0400

    refactor: ref is automatically added to deps

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ import {
 const Dropzone = forwardRef(({ children, ...params }, ref) => {
   const { open, ...props } = useDropzone(params)
 
-  useImperativeHandle(ref, () => ({ open }), [ref, open])
+  useImperativeHandle(ref, () => ({ open }), [open])
 
   // TODO: Figure out why react-styleguidist cannot create docs if we don't return a jsx element
   return <Fragment>{children({ ...props, open })}</Fragment>
